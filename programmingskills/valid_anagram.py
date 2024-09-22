7a57923126e5ea10aa05c8965fdc0e617ac303f3
Prithviraj Gadgi <prithvirajgadgi@gmail.com>
2024-09-22 12:34:02 +0530

    Added valid_anagram.py

--- programmingskills/valid_anagram.py
+++ programmingskills/valid_anagram.py
@@ -1,11 +1,10 @@
+from collections import Counter
+
+
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        for i in max(s, t):
-            if s.count(i) != t.count(i):
-                return False
-
-        return True
+        return Counter(s) == Counter(t)
 
 
 solution = Solution()
-print(solution.is_anagram("rat", "car"))
+print(solution.is_anagram("aba", "aab"))
